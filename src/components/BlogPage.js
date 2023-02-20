adc70a976a21819fa299209818817ecfb995ec3d
kaneko-taiyo <taiyo.2357it@gmail.com>
2023-02-20 13:12:28 +0900

    jvascript reactを使ってweb制作

---
+++ src/components/BlogPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className='container'>
+        <h1>ブログ</h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores sed nisi, id voluptatem non, repudiandae dolorem iste nulla recusandae a sequi nostrum perspiciatis autem voluptas quas quaerat unde possimus ad ut. Voluptas ipsa error, esse soluta voluptatum fuga pariatur beatae eaque id, deleniti consequatur quas mollitia incidunt? Autem atque iure odio, harum modi iste ratione eaque est, recusandae facilis officiis id perferendis veritatis nam possimus laborum consequatur molestias non inventore ab. Ipsa autem rem in totam quia incidunt molestias quod dignissimos provident quo blanditiis similique nemo consequuntur vel explicabo repellat rerum sint assumenda consequatur, possimus deserunt ipsam. Distinctio, illum animi!
+        </p>
+    </div>
+  );
+}
+
+export default BlogPage
